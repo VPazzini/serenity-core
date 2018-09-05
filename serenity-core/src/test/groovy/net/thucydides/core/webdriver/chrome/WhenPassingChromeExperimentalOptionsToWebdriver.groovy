4de77a4e9ed26b25ff70f543ce00808062175508
Vinicius Pazzini <pazzinivinicius@gmail.com>
2018-09-05 20:44:42 +0200

    fixing tests, using mock environment variables

--- serenity-core/src/test/groovy/net/thucydides/core/webdriver/chrome/WhenPassingChromeExperimentalOptionsToWebdriver.groovy
+++ serenity-core/src/test/groovy/net/thucydides/core/webdriver/chrome/WhenPassingChromeExperimentalOptionsToWebdriver.groovy
@@ -1,39 +1,32 @@
 package net.thucydides.core.webdriver.chrome
 
 import net.serenitybdd.core.webdriver.driverproviders.ChromeDriverCapabilities
-import net.thucydides.core.guice.Injectors
-import net.thucydides.core.util.EnvironmentVariables
+import net.thucydides.core.util.MockEnvironmentVariables
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class WhenPassingChromeExperimentalOptionsToWebdriver extends Specification {
 
     @Unroll
-    def "should accept experimental options"() {
+    def "Chrome experimental options should be added to chrome Capabilities"() {
         given:
-        System.setProperty("chrome_experimental_options.test1", "test1");
-        System.setProperty("chrome_experimental_options.test_boolean", "false");
-        System.setProperty("chrome_experimental_options.test_int", "5");
-        System.setProperty("chrome_experimental_option.test_false", "test1");
-        System.setProperty("chrome_experimental_options.test1.test2", "some bigger value with a lot of characters");
-        def chromeCap = new ChromeDriverCapabilities(Injectors.getInjector().getProvider(EnvironmentVariables.class).get(), "");
+        System.setProperty(optionText, optionValue.toString());
+        def chromeCap = new ChromeDriverCapabilities(MockEnvironmentVariables.fromSystemEnvironment(), "");
         when:
         def options = chromeCap.getCapabilities()
         then:
-
-        (options.getCapability("goog:chromeOptions") as TreeMap<String, Object>).containsKey("test1")
-        ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).get("test1") == "test1"
-        ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).get("test1") instanceof String
-
-        ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).containsKey("test1.test2")
-        ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).get("test1.test2") == "some bigger value with a lot of characters"
-
-        !((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).containsKey("test_false")
-
-        ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).get("test_boolean") == false
-        ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).get("test_int") instanceof Integer
-
-
+        def exp_opt = optionText.replaceAll("chrome_experimental_options.", "")
+        ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).containsKey(exp_opt) == shouldContain
+        if (shouldContain) {
+            ((TreeMap<String, Object>) options.getCapability("goog:chromeOptions")).get(exp_opt) == optionValue
+        }
+        where:
+        optionText                                | optionValue                   | shouldContain
+        "chrome_experimental_options.test1"       | "String"                      | true
+        "chrome_experimental_options.test2"       | "Other String that is bigger" | true
+        "chrome_experimental_options.test3"       | true                          | true
+        "chrome_experimental_options.test4.test1" | 4                             | true
+        "chrome_preferences.should_not_be_added"  | false                         | false
     }
 
 
